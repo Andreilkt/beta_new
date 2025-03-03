812c7f2e986591a0f3bac4ae284619c090af3da1
Савчук Андрей <a.savchuk@netrika.ru>
2025-03-03 13:10:08 +0700

    изменена settings.py

--- betaServis/settings.py
+++ betaServis/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = (BASE_DIR / 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = (BASE_DIR / 'static')
 # STATICFILES_DIRS = [
 #     BASE_DIR /
 #     "modules/beta/static/bootstrap"
