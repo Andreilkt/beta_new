1110030ec841b0bcf3386341ff2c403afb0fd90d
Савчук Андрей <a.savchuk@netrika.ru>
2025-03-03 12:31:57 +0700

    изменена settings.py

--- betaServis/settings.py
+++ betaServis/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%-%h*!x1h00e1!i)z)pqyut0%^tynq#n$tdug@4-4$w-ekpq^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['45.67.32.32']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,9 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
-STATIC_URL = 'static/'
-STATIC_ROOT = (BASE_DIR / 'static')
+STATIC_URL = '/static/'
+#STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = (BASE_DIR / 'static')
 # STATICFILES_DIRS = [
 #     BASE_DIR /
 #     "modules/beta/static/bootstrap"
