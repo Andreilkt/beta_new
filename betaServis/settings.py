b5bd5f504f4af1e5ced87d7b3bbf00ce85b7a394
Савчук Андрей <a.savchuk@netrika.ru>
2025-03-03 13:36:49 +0700

    изменена settings.py

--- betaServis/settings.py
+++ betaServis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%-%h*!x1h00e1!i)z)pqyut0%^tynq#n$tdug@4-4$w-ekpq^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.67.32.32']
 
 
 # Application definition
