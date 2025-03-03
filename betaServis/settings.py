615fff78db40382c7c6609b43a3ca44c2994b7e1
Савчук Андрей <a.savchuk@netrika.ru>
2025-03-03 14:15:27 +0700

    изменена settings.py

--- betaServis/settings.py
+++ betaServis/settings.py
@@ -122,20 +122,21 @@ USE_TZ = True
 #STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = "/var/www/beta_new/static/"
+#STATIC_ROOT = "/var/www/beta_new/static/"
 
 
 
-#STATIC_ROOT = (BASE_DIR / 'static')
+STATIC_ROOT = (BASE_DIR / 'static')
 # STATICFILES_DIRS = [
 #     BASE_DIR /
 #     "modules/beta/static/bootstrap"
 # ]
 
 #STATICFILES_DIRS = [BASE_DIR / 'templates']
-#MEDIA_ROOT = (BASE_DIR / 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = "/var/www/beta-new/media/"
+MEDIA_ROOT = (BASE_DIR / 'media')
+
+#MEDIA_ROOT = "/var/www/beta-new/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
